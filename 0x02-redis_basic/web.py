c66cb72d37b8cdb0ec9f0db7db6c7556e178f583
Ralph Okorie <okorieralphc@gmail.com>
2024-08-15 02:17:14 +0000

    updated

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,38 +2,35 @@
 """
 web cache and tracker
 """
-
 import requests
 import redis
 from functools import wraps
 
-# Initialize the Redis connection
 store = redis.Redis()
 
+
 def count_url_access(method):
-    """ Decorator counting how many times a URL is accessed """
+    """ Decorator counting how many times
+    a URL is accessed """
     @wraps(method)
     def wrapper(url):
-        cached_key = f"cached:{url}"
+        cached_key = "cached:" + url
         cached_data = store.get(cached_key)
-        
-        # If data is in cache, return it
         if cached_data:
             return cached_data.decode("utf-8")
 
-        # If not, increment the count and fetch the data
-        count_key = f"count:{url}"
-        store.incr(count_key)
+        count_key = "count:" + url
         html = method(url)
-        
-        # Store the fetched data in cache with an expiration of 10 seconds
-        store.setex(cached_key, 10, html)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
         return html
     return wrapper
+
 
 @count_url_access
 def get_page(url: str) -> str:
     """ Returns HTML content of a url """
     res = requests.get(url)
     return res.text
-
